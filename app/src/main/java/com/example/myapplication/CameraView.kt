23b73ad2f43ce28fd6196c0bb722653eae925370
suneettipirneni <suneettipirneni@icloud.com>
2023-12-01 13:36:15 -0500

    initial commit

---
+++ app/src/main/java/com/example/myapplication/CameraView.kt
@@ -0,0 +1,194 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageFormat
+import android.graphics.Rect
+import android.graphics.YuvImage
+import android.net.Uri
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
+import android.util.Size
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.Lens
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import org.pytorch.Tensor
+import org.pytorch.torchvision.TensorImageUtils
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.Executor
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+import kotlin.system.measureTimeMillis
+
+
+private fun takePhoto(filenameFormat: String, imageCapture: ImageCapture, outputDirectory: File, executor: Executor, onImageCaptured: (Uri) -> Unit, onError: (ImageCaptureException) -> Unit) {
+    val photoFile = File(
+        outputDirectory,
+        SimpleDateFormat(filenameFormat, Locale.US).format(System.currentTimeMillis()) + ".jpg"
+    )
+
+    val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+    imageCapture.takePicture(outputOptions, executor, object: ImageCapture.OnImageSavedCallback {
+        override fun onError(exception: ImageCaptureException) {
+            Log.e("cybertruck", "Had an issue taking the photo")
+            onError(exception)
+        }
+
+        override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+            val savedUri = Uri.fromFile(photoFile)
+            onImageCaptured(savedUri)
+        }
+    })
+}
+
+private suspend fun Context.getCameraProvider(): ProcessCameraProvider = suspendCoroutine { continuation ->
+    ProcessCameraProvider.getInstance(this).also { cameraProvider ->
+        cameraProvider.addListener({
+            continuation.resume(cameraProvider.get())
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+
+}
+
+private fun imgToBitmap(image: ImageProxy): Bitmap {
+    val planes = image.planes
+    val yBuffer = planes[0].buffer
+    val uBuffer = planes[1].buffer
+    val vBuffer = planes[2].buffer
+    val ySize = yBuffer.remaining()
+    val uSize = uBuffer.remaining()
+    val vSize = vBuffer.remaining()
+    val nv21 = ByteArray(ySize + uSize + vSize)
+    yBuffer[nv21, 0, ySize]
+    vBuffer[nv21, ySize, vSize]
+    uBuffer[nv21, ySize + vSize, uSize]
+    val yuvImage = YuvImage(nv21, ImageFormat.NV21, image.width, image.height, null)
+    val out = ByteArrayOutputStream()
+    yuvImage.compressToJpeg(Rect(0, 0, yuvImage.width, yuvImage.height), 75, out)
+    val imageBytes = out.toByteArray()
+    return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+}
+
+@Composable
+@androidx.annotation.OptIn(androidx.camera.core.ExperimentalGetImage::class)
+fun  CameraView(outputDirectory: File, executor: Executor, onImageCaptured: (Uri) -> Unit, onError: (ImageCaptureException) -> Unit, onFrame: (Tensor) -> Unit) {
+    val lensFacing = CameraSelector.LENS_FACING_FRONT
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    val preview = Preview.Builder().build()
+    val previewView = remember { PreviewView(context) }
+    val imageCapture: ImageCapture = remember { ImageCapture.Builder().build() }
+
+    val cameraSelector = CameraSelector.Builder()
+        .build()
+
+    val imageAnalysis = ImageAnalysis.Builder()
+        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+        .setTargetResolution(Size(400, 224))
+        .build()
+
+    var latestAnalyzedTimestamp = 0L;
+    val maxFps = 10;
+
+    val backgroundThread = HandlerThread("ImageAnalysisThread").apply { start() }
+    val backgroundHandler = Handler(backgroundThread.looper)
+
+    imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(context)) { imageProxy ->
+
+            val elapsedAnalysisTime = measureTimeMillis {
+                // Process the image frames here
+                val mInputTensorBuffer =
+                    Tensor.allocateFloatBuffer(3 * 224 * 224)
+                val mInputTensor = Tensor.fromBlob(
+                    mInputTensorBuffer,
+                    longArrayOf(1, 3, 224, 224)
+                )
+
+                TensorImageUtils.imageYUV420CenterCropToFloatBuffer(imageProxy.image, imageProxy.imageInfo.rotationDegrees, 224, 224, floatArrayOf(0.3357F,0.3277F,0.3343F),
+                    floatArrayOf(0.3447F,0.3375F, 0.3347F),
+                    mInputTensorBuffer, 0)
+
+                onFrame(mInputTensor)
+
+                // Close the imageProxy to free up resources
+                imageProxy.close()
+            }
+
+            imageProxy.use {
+                if (elapsedAnalysisTime < 1000) {
+                    Thread.sleep(1000 - elapsedAnalysisTime)
+                }
+            }
+    }
+
+    LaunchedEffect(lensFacing) {
+        val cameraProvider = context.getCameraProvider()
+        cameraProvider.unbindAll()
+        cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview, imageCapture, imageAnalysis)
+
+        preview.setSurfaceProvider(previewView.surfaceProvider)
+    }
+
+    Box(contentAlignment = Alignment.BottomCenter, modifier = Modifier.fillMaxHeight()) {
+        AndroidView({ previewView }, modifier = Modifier.fillMaxWidth())
+
+        IconButton(modifier = Modifier.padding(bottom = 20.dp), onClick = {
+            Log.i("cybertruck", "on click")
+            takePhoto(
+                filenameFormat = "yyyy-MM-dd-HH-mm-ss-SSS",
+                imageCapture = imageCapture,
+                outputDirectory = outputDirectory,
+                executor = executor,
+                onImageCaptured = onImageCaptured,
+                onError = onError
+            )
+        }, content = {
+            Icon(
+                imageVector = Icons.Sharp.Lens,
+                contentDescription = "Take picture",
+                tint = Color.White,
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(1.dp)
+                    .border(1.dp, Color.White, CircleShape)
+            )
+        })
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,334 @@
+package com.example.myapplication
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.content.res.AssetManager
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.*
+import androidx.compose.material3.TopAppBarDefaults.smallTopAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import org.pytorch.IValue
+import org.pytorch.LiteModuleLoader
+import org.pytorch.torchvision.TensorImageUtils
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
+import java.util.Arrays
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+
+class MainActivity : ComponentActivity() {
+
+    @Throws(IOException::class)
+    private fun getModelByteBuffer(assetManager: AssetManager, modelPath: String): MappedByteBuffer {
+        val fileDescriptor = assetManager.openFd(modelPath)
+        val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
+        val fileChannel = inputStream.channel
+        val startOffset = fileDescriptor.startOffset
+        val declaredLength = fileDescriptor.declaredLength
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
+    }
+
+
+    private val requestCameraPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {isGranted ->
+        if (isGranted) {
+            Log.i("info", "permission granted")
+        } else {
+            Log.i("info", "Permission Denied")
+        }
+    }
+
+    private fun requestCameraPermission() {
+        when {
+            ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED -> {
+                Log.i("cybertruck", "Permission previously granted")
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA) -> {
+                Log.i("cybertruck", "Show camera permissions dialog")
+            }
+
+            else -> {
+                requestCameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+            }
+        }
+    }
+
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
+    private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
+    private var activeClass: MutableState<String> = mutableStateOf("None")
+    private var secondClass: MutableState<String> = mutableStateOf("None")
+    private var thirdClass: MutableState<String> = mutableStateOf("None")
+    private var fourthClass: MutableState<String> = mutableStateOf("None")
+    private var fifthClass: MutableState<String> = mutableStateOf("None")
+
+    private fun handleImageCapture(uri: Uri) {
+        Log.i("cybertruck", "Image captured: $uri")
+        shouldShowCamera.value = false
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+
+        return if (mediaDir != null && mediaDir.exists()) mediaDir else filesDir
+    }
+
+//    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            if (shouldShowCamera.value) {
+//                CameraView(
+//                    outputDirectory = outputDirectory,
+//                    executor = cameraExecutor,
+//                    onImageCaptured = ::handleImageCapture,
+//                    onError = { Log.e("kilo", "View error:", it) }
+//                )
+//            }
+//        }
+//
+//        requestCameraPermission()
+//
+//        outputDirectory = getOutputDirectory()
+//        cameraExecutor = Executors.newSingleThreadExecutor()
+//    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    /**
+     * Copies specified asset to the file in /files app directory and returns this file absolute path.
+     *
+     * @return absolute file path
+     */
+    @Throws(IOException::class)
+    fun assetFilePath(context: Context, assetName: String?): String? {
+        val file = File(context.filesDir, assetName)
+        if (file.exists() && file.length() > 0) {
+            return file.absolutePath
+        }
+        context.assets.open(assetName!!).use { `is` ->
+            FileOutputStream(file).use { os ->
+                val buffer = ByteArray(4 * 1024)
+                var read: Int
+                while (`is`.read(buffer).also { read = it } != -1) {
+                    os.write(buffer, 0, read)
+                }
+                os.flush()
+            }
+            return file.absolutePath
+        }
+    }
+
+    fun softmax(logits: FloatArray): FloatArray {
+        val expValues = logits.map { kotlin.math.exp(it) }
+        val sumExp = expValues.sum()
+
+        return expValues.map { it / sumExp }.toFloatArray()
+    }
+
+    fun topK(a: FloatArray, topk: Int): IntArray {
+        val values = FloatArray(topk)
+        Arrays.fill(values, -Float.MAX_VALUE)
+        val ixs = IntArray(topk)
+        Arrays.fill(ixs, -1)
+        for (i in a.indices) {
+            for (j in 0 until topk) {
+                if (a[i] > values[j]) {
+                    for (k in topk - 1 downTo j + 1) {
+                        values[k] = values[k - 1]
+                        ixs[k] = ixs[k - 1]
+                    }
+                    values[j] = a[i]
+                    ixs[j] = i
+                    break
+                }
+            }
+        }
+        return ixs
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+
+        val bitmap = BitmapFactory.decodeStream(assets.open("other.jpg"))
+        val module = LiteModuleLoader.load(assetFilePath(this, "smaller.ptl"))
+
+        val inputTensor = TensorImageUtils.bitmapToFloat32Tensor(
+            bitmap,
+            TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB
+        )
+
+        val outputTensor = module.forward(IValue.from(inputTensor)).toTensor()
+        val scores: FloatArray = outputTensor.getDataAsFloatArray()
+
+        var maxScore = -Float.MAX_VALUE
+        var maxScoreIdx = -1
+        for (i in scores.indices) {
+            if (scores[i] > maxScore) {
+                maxScore = scores[i]
+                maxScoreIdx = i
+            }
+        }
+
+//                                    Log.i("cybertruck", labels[maxScoreIdx])
+        Log.i("cybertruck", maxScoreIdx.toString())
+
+        Log.i("cybertruck", scores.contentToString())
+
+        val labels = listOf("Safe Driving", "Text Right", "Phone Right", "Text Left", "Phone Left", "Adjusting Radio", "Drinking", "Reaching Behind", "Hair or Makeup", "Talking to Passenger")
+
+        setContent {
+                MyApplicationTheme {
+
+                        CameraView(
+                    outputDirectory = outputDirectory,
+                    executor = cameraExecutor,
+                    onImageCaptured = ::handleImageCapture,
+                    onError = { Log.e("kilo", "View error:", it) },
+                            onFrame = { bmap ->
+
+
+
+                                cameraExecutor.execute {
+                                    val matrix = Matrix()
+
+
+
+                                    val outputTensor = module.forward(IValue.from(bmap)).toTensor()
+                                    val scores: FloatArray = softmax(outputTensor.dataAsFloatArray)
+
+
+
+                                    var maxScore = -Float.MAX_VALUE
+                                    var maxScoreIdx = -1
+                                    for (i in scores.indices) {
+                                        if (scores[i] > maxScore) {
+                                            maxScore = scores[i]
+                                            maxScoreIdx = i
+                                        }
+                                    }
+
+                                    Log.i("cybertruck", labels[maxScoreIdx])
+                                    Log.i("cybertruck", scores.contentToString())
+
+                                    val topScores = topK(scores, 5)
+                                    
+                                    activeClass.value = labels[maxScoreIdx]
+                                    secondClass.value = labels[topScores[1]]
+                                    thirdClass.value = labels[topScores[2]]
+                                    fourthClass.value = labels[topScores[3]]
+                                    fifthClass.value = labels[topScores[4]]
+
+
+                                }
+
+
+                            }
+              )
+                    BottomAppBar {
+                        Row(
+                            Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            Text("1. ${activeClass.value}", fontSize = 30.sp, modifier = Modifier.padding(10.dp))
+                            Text("2. ${secondClass.value}", modifier = Modifier.padding(10.dp))
+                            Text("3. ${thirdClass.value}", modifier = Modifier.padding(10.dp))
+                            Text("4. ${fourthClass.value}", modifier = Modifier.padding(10.dp))
+                            Text("5. ${fifthClass.value}", modifier = Modifier.padding(10.dp))
+                        }
+
+                    }
+
+                }
+            }
+
+        requestCameraPermission()
+
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun AppScaffold() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("CyberTruck")
+                }
+            )
+        },
+    ) { innerPadding ->
+        Body(innerPadding)
+    }
+}
+
+}
+
+@Composable
+fun Body(innerPadding : PaddingValues) {
+    Column(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "test")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
